b609799321c959362bfbf7e83fb157ec033a1d7d
olami851 <abdhamid851@gmail.com>
2025-01-07 16:48:09 +0100

    Shopping List Manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,51 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add item to shopping list")
+    print("2. Remove item from shopping list")
+    print("3. View items in shopping list")
+    print("4. Exit shopping list")
+
+def add_item(shopping_list):
+    item = input("Enter the name of the item to add to shopping list: ").strip()
+    if item:
+        shopping_list.append(item)
+        print(f"'{item}' has been added to shopping list")
+    else:
+        print("Item name can't be empty")
+
+def remove_item(shopping_list):
+    item = input("Enter the name of the item to remove from shopping list: ").strip()
+    if item in shopping_list:
+        shopping_list.remove(item)
+        print(f"'{item}' has been removed from shopping list")
+    else:
+        print(f"'{item}' is not in shopping list")
+
+def view_list(shopping_list):
+    if shopping_list:
+        print("Current shopping list:")
+        for number, item in enumerate(shopping_list, start=1):
+             print(f"{number}. {item}")
+    else:
+        print("Shopping list is currently empty")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Choose an option (1-4): ").strip()
+        if choice == '1':
+            add_item(shopping_list)
+        elif choice == '2':
+            remove_item(shopping_list)
+        elif choice == '3':
+            view_list(shopping_list)
+        elif choice == '4':
+            print("Exit the shopping list manager")
+            break
+        else:
+            print("Invalid choice ensure choice is between (1-4)")
+if __name__ == '__main__':
+    main()
+
+
