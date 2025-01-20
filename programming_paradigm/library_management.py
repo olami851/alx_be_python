dd21fd6846f241fde28716dbabe712a53dcb7b04
olami851 <abdhamid851@gmail.com>
2025-01-20 09:53:57 +0100

    Implementing Basic OOP for a Library Management System

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,53 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as available."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Check if the book is available."""
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        """Add a book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by title."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                print(f"Checked out '{title}'")
+                return
+        print(f"Sorry, '{title}' is not available.")
+
+    def return_book(self, title):
+        """Return a book by title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                print(f"Returned '{title}'")
+                return
+        print(f"Sorry, '{title}' is not checked out.")
+
+    def list_available_books(self):
+        """List all available books in the library."""
+        available_books = [book for book in self._books if book.is_available()]
+        if not available_books:
+            print("No books are currently available.")
+        else:
+            for book in available_books:
+                print(f"{book.title} by {book.author}")
